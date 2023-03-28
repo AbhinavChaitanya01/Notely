3e5861fe5ab612e0c71fa15887ad162b2c4b231f
Abhinav Chaitanya <98750072+AbhinavChaitanya01@users.noreply.github.com>
2023-03-28 10:08:19 +0530

    updates for deployment requisites

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 require("dotenv").config();
 const connectToMongo=()=>{
     mongoose.connect(`mongodb+srv://abhinavchaitanyadtu:${process.env.DBPASS}@cluster0.ymevs4r.mongodb.net/notelyDB`,{useNewUrlParser: true},()=>{
-        console.log("Connected to mongo successfully");
     });
 }
 module.exports = connectToMongo;
